29331a7d9230f4781f3b1ed443534a51bf2c81ee
nicolasmadridstel <79945356+nicolasmadridstel@users.noreply.github.com>
2021-03-03 22:32:15 -0300

    Add files via upload

---
+++ aula 2/src/variavel.java
@@ -0,0 +1,74 @@
+/*
+ * variabel real de A.
+ * 
+ * 
+ */
+package Aula2;
+//classe Scanner é responsável pela leitura do dados digitados no teclado
+//a classe Scanner é predefinida no Java
+
+import java.io.InputStream;
+import java.util.Scanner;
+
+
+public class variavel {
+
+    private static Scanner Scanner(InputStream in) {
+    int x;
+    package defalt;
+//classe Scanner é responsável pela leitura do dados digitados no teclado
+//a classe Scanner é predefinida no Java
+import java.util.Scanner;static Scanner ler = variavel(System.in);
+    
+   
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("De valor a variavel de A:");
+        int x = ler.nextInt();
+        System.out.println("O dobro de A: " + (x*2));
+        System.out.println("O triplo do dobro de A: " + ((x*2)*3));
+        System.out.println("A metade do triplo do dobro de A: " + (((x*2)*3)/2));
+        System.out.println("O quadrado da variavel A: " + (x*x));
+        System.out.println("A quinta parte do dobro de A: " + ((x*2)/5));
+        
+       
+        
+    }
+
+    private static variavel(InputStream in) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private static variavel(InputStream in) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private static variavel(InputStream in) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private static variavel(InputStream in) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private static variavel(InputStream in) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private static variavel(InputStream in) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private static variavel(InputStream in) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private static variavel(InputStream in) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
